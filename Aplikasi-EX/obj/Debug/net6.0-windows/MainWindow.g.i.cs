067ec4cf7e5cf78f75f0e0f71c8359a68ea4a4b0
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-09-05 18:38:52 +0700

    chore: add initial folder structure for project setup <fix>

--- Aplikasi-EX/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Aplikasi-EX/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Aplikasi_EX {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Aplikasi-EX;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Aplikasi-EX;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
