7a09a000b2e59e5f175daaf716fc5f85e2119865
MuhammadFajrulfalaqIS <117189822+MuhammadFajrulfalaqIS@users.noreply.github.com>
2024-10-27 13:54:04 +0700

    feat: add encapsulation method in SignIn

--- Aplikasi-EX/SignInWindow.xaml.cs
+++ Aplikasi-EX/SignInWindow.xaml.cs
@@ -19,9 +19,64 @@ namespace Aplikasi_EX
     /// </summary>
     public partial class SignInWindow : Window
     {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username;}
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _username = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Username tidak boleh kosong.");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Password tidak boleh kosong.");
+                }
+            }
+        }
         public SignInWindow()
         {
             InitializeComponent();
+        }
+
+        private bool ValidateLogin()
+        {
+            return Username == "admin" && Password == "12345";
+        }
+
+        public void Login()
+        {
+            Username = txtUsername.Text;
+            Password = txtPassword.Text;
+
+            if (ValidateLogin()) 
+            {
+                MessageBox.Show("Login berhasil!");
+                Open_MainWindow(this, new RoutedEventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password salah.");
+            }
         }
 
         private void Open_SignUp(object sender, RoutedEventArgs e)
@@ -36,6 +91,41 @@ namespace Aplikasi_EX
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             this.Close();
+        }
+
+        private void SignInButton_Click (object sender, RoutedEventArgs e)
+        {
+            Login();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox.Name == "txtUsername" && textBox.Text == "Masukkan emailmu")
+            {
+                textBox.Text = "";
+            }
+            else if (textBox.Name == "txtPassword" && textBox.Text == "Masukkan passwordmu")
+            {
+                textBox.Text = "";
+                textBox.Visibility = Visibility.Visible;
+                textBox.Focus();
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox.Name == "txtUsername" && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Masukkan emailmu";
+            }
+            else if (textBox.Name == "txtPassword" && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Masukkan passwordmu";
+            }
         }
     }
 }
