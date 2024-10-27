f2908e28cb3246fa54dd3129f38db38bf82b60b8
rhnathar <zildan.athar@gmail.com>
2024-10-27 17:00:37 +0700

    fix directory

--- Aplikasi-EX/obj/Debug/net6.0-windows/Aplikasi-EX.AssemblyInfo.cs
+++ Aplikasi-EX/obj/Debug/net6.0-windows/Aplikasi-EX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplikasi-EX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a09a000b2e59e5f175daaf716fc5f85e2119865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplikasi-EX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplikasi-EX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aplikasi-EX/obj/Debug/net6.0-windows/App.g.i.cs
+++ Aplikasi-EX/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Aplikasi_EX {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Aplikasi-EX;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Aplikasi-EX;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
