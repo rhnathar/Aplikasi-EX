b6d45f36f3dbb82c0ca141087479def8c1e5e5ab
rhnathar <zildan.athar@gmail.com>
2024-11-03 18:31:44 +0700

    feat: add data access layer for user

---
+++ Aplikasi-EX/DataAccess/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿// DAL/Interfaces/IUserRepository.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aplikasi_EX.Model;
+
+public interface IUserRepository
+{
+    Task RegisterAsync(User user);
+    Task<User> LoginAsync(string email, string password);
+    Task<List<User>> GetUsersAsync();
+}
--- Aplikasi-EX/DataAccess/UserRepository.cs
+++ Aplikasi-EX/DataAccess/UserRepository.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿// DAL/Repositories/UserRepository.cs
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-using Aplikasi_EX.Utilities;
-using Aplikasi_EX.Model;
 using Npgsql;
+using Aplikasi_EX.Model;
+using Aplikasi_EX.Utilities;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
     private readonly string _connectionString;
 
@@ -17,18 +14,68 @@ public class UserRepository
         _connectionString = ConfigurationHelper.GetConnectionString("AzureDbConnection");
     }
 
-    public List<User> GetUsers()
+    public async Task RegisterAsync(User user)
+    {
+        using (var conn = new NpgsqlConnection(_connectionString))
+        {
+            await conn.OpenAsync();
+            string query = "CALL RegisterUser(@name, @address, @phone_number, @email, @password, @type)";
+            using (var cmd = new NpgsqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@name", user.Name);
+                cmd.Parameters.AddWithValue("@address", user.Address);
+                cmd.Parameters.AddWithValue("@phone_number", user.PhoneNumber);
+                cmd.Parameters.AddWithValue("@email", user.Email);
+                cmd.Parameters.AddWithValue("@password", user.Password);
+                cmd.Parameters.AddWithValue("@type", user.Type);
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+    }
+
+    public async Task<User> LoginAsync(string email, string password)
+    {
+        User user = null;
+        using (var conn = new NpgsqlConnection(_connectionString))
+        {
+            await conn.OpenAsync();
+            string query = "SELECT * FROM LoginUser(@Email, @Password)";
+            using (var cmd = new NpgsqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        user = new User
+                        {
+                            UserID = reader.GetInt32(reader.GetOrdinal("userID")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            Address = reader.GetString(reader.GetOrdinal("address")),
+                            PhoneNumber = reader.GetString(reader.GetOrdinal("phone_number")),
+                            Email = reader.GetString(reader.GetOrdinal("email")),
+                            Type = reader.GetString(reader.GetOrdinal("type"))
+                        };
+                    }
+                }
+            }
+        }
+        return user;
+    }
+
+    public async Task<List<User>> GetUsersAsync()
     {
         var users = new List<User>();
         using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
         {
-            conn.Open();
+            await conn.OpenAsync();
             string query = "SELECT * FROM user_account";
             using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
             {
-                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
                 {
-                    while (reader.Read())
+                    while (await reader.ReadAsync())
                     {
                         users.Add(new User
                         {
