739f97981eb2b790a264c6572ce3aa722cc5f378
rhnathar <zildan.athar@gmail.com>
2024-11-03 14:56:04 +0700

    feat: add Models

---
+++ Aplikasi-EX/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_EX.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Description { get; set; }
+        public byte[] ProductImage { get; set; }
+        public int SellerID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Aplikasi-EX/Model/User.cs
+++ Aplikasi-EX/Model/User.cs
@@ -13,5 +13,7 @@ namespace Aplikasi_EX.Model
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
     }
 }
