70a3dc410fffd91d7b1b5b5679e274915fe2c2fc
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-11-04 12:51:51 +0700

    feat: try to make detail product into usercontrol and bind the data

---
+++ Aplikasi-EX/View/DetailProduct.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aplikasi_EX.ViewModel;
+
+
+namespace Aplikasi_EX.View
+{
+    /// <summary>
+    /// Interaction logic for DetailProduct.xaml
+    /// </summary>
+    public partial class DetailProduct : UserControl
+    {
+        public DetailProduct()
+        {
+            InitializeComponent();
+            DataContext = new DetailProductVM(); // Using dummy data
+        }
+    }
+}
---
+++ Aplikasi-EX/ViewModel/DetailProductVM.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Aplikasi_EX.ViewModel
+{
+    public class DetailProductVM : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _productName;
+        private string _condition;
+        private string _category;
+        private int _stock;
+        private string _location;
+        private decimal _price;
+        private string _description;
+        private string _productImage;
+
+        public string ProductName
+        {
+            get => _productName;
+            set { _productName = value; OnPropertyChanged(nameof(ProductName)); }
+        }
+
+        public string Condition
+        {
+            get => _condition;
+            set { _condition = value; OnPropertyChanged(nameof(Condition)); }
+        }
+
+        public string Category
+        {
+            get => _category;
+            set { _category = value; OnPropertyChanged(nameof(Category)); }
+        }
+
+        public int Stock
+        {
+            get => _stock;
+            set { _stock = value; OnPropertyChanged(nameof(Stock)); }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set { _location = value; OnPropertyChanged(nameof(Location)); }
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set { _price = value; OnPropertyChanged(nameof(Price)); }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(nameof(Description)); }
+        }
+
+        public string ProductImage
+        {
+            get => _productImage;
+            set { _productImage = value; OnPropertyChanged(nameof(ProductImage)); }
+        }
+
+        public DetailProductVM()
+        {
+            // Dummy data for testing
+            ProductName = "ASUS ROG RTX 3070 Ti";
+            Condition = "Bekas";
+            Category = "Barang Elektronik";
+            Stock = 5;
+            Location = "Bogor";
+            Price = 5000000;
+            Description = "Powerful GPU suitable for gaming and professional work.";
+            ProductImage = "/img/rtx.png"; // Relative path to image
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
