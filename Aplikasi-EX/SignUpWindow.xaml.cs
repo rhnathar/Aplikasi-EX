bd5c313bd21d2557d44c46e0de3888dca5985212
MuhammadFajrulfalaqIS <117189822+MuhammadFajrulfalaqIS@users.noreply.github.com>
2024-11-10 09:17:09 +0700

    fix: connect data context to SignUpVM

--- Aplikasi-EX/SignUpWindow.xaml.cs
+++ Aplikasi-EX/SignUpWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace Aplikasi_EX
 {
@@ -22,6 +24,7 @@ namespace Aplikasi_EX
         public SignUpWindow()
         {
             InitializeComponent();
+            DataContext = new Aplikasi_EX.ViewModel.SignUpVM(); // Atur DataContext ke SignUpVM
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
