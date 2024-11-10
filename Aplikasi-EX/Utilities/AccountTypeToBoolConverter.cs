84a9b6d1ac76a4e7e6dbbc6478b3c43a1bb89ef4
MuhammadFajrulfalaqIS <117189822+MuhammadFajrulfalaqIS@users.noreply.github.com>
2024-11-10 09:12:43 +0700

    feat: add AccountTypeToBoolConverter

---
+++ Aplikasi-EX/Utilities/AccountTypeToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Aplikasi_EX.Utilities
+{
+    public class AccountTypeToBoolConverter : IValueConverter
+    {
+        // Convert from AccountType (string) to IsChecked (bool)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            return value.ToString() == parameter.ToString(); // Compare the value with the parameter
+        }
+
+        // Convert back from IsChecked (bool) to AccountType (string)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? parameter.ToString() : null; // Return the corresponding account type
+        }
+    }
+
+}
