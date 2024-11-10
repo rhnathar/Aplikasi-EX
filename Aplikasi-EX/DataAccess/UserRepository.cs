5a66eaceaa0186e9caefe09911955a9d5a906197
MuhammadFajrulfalaqIS <117189822+MuhammadFajrulfalaqIS@users.noreply.github.com>
2024-11-10 09:18:33 +0700

    fix: fixing user repository command

--- Aplikasi-EX/DataAccess/UserRepository.cs
+++ Aplikasi-EX/DataAccess/UserRepository.cs
@@ -4,91 +4,102 @@ using System.Threading.Tasks;
 using Npgsql;
 using Aplikasi_EX.Model;
 using Aplikasi_EX.Utilities;
+using System;
 
-public class UserRepository : IUserRepository
+namespace Aplikasi_EX.DataAccess
 {
-    private readonly string _connectionString;
-
-    public UserRepository()
+    public class UserRepository : IUserRepository
     {
-        _connectionString = ConfigurationHelper.GetConnectionString("AzureDbConnection");
-    }
+        private readonly string _connectionString;
 
-    public async Task RegisterAsync(User user)
-    {
-        using (var conn = new NpgsqlConnection(_connectionString))
+        public UserRepository()
         {
-            await conn.OpenAsync();
-            string query = "CALL RegisterUser(@name, @address, @phone_number, @email, @password, @type)";
-            using (var cmd = new NpgsqlCommand(query, conn))
-            {
-                cmd.Parameters.AddWithValue("@name", user.Name);
-                cmd.Parameters.AddWithValue("@address", user.Address);
-                cmd.Parameters.AddWithValue("@phone_number", user.PhoneNumber);
-                cmd.Parameters.AddWithValue("@email", user.Email);
-                cmd.Parameters.AddWithValue("@password", user.Password);
-                cmd.Parameters.AddWithValue("@type", user.Type);
-                await cmd.ExecuteNonQueryAsync();
-            }
+            _connectionString = ConfigurationHelper.GetConnectionString("AzureDbConnection");
         }
-    }
 
-    public async Task<User> LoginAsync(string email, string password)
-    {
-        User user = null;
-        using (var conn = new NpgsqlConnection(_connectionString))
+        public async Task RegisterAsync(User user)
         {
-            await conn.OpenAsync();
-            string query = "SELECT * FROM LoginUser(@Email, @Password)";
-            using (var cmd = new NpgsqlCommand(query, conn))
+            try
             {
-                cmd.Parameters.AddWithValue("@Email", email);
-                cmd.Parameters.AddWithValue("@Password", password);
-                using (var reader = await cmd.ExecuteReaderAsync())
+                using (var conn = new NpgsqlConnection(_connectionString))
                 {
-                    if (await reader.ReadAsync())
+                    await conn.OpenAsync();
+                    string query = "SELECT RegisterUser(@name, @address, @phone_number, @email, @password, @type)";
+                    using (var cmd = new NpgsqlCommand(query, conn))
                     {
-                        user = new User
-                        {
-                            UserID = reader.GetInt32(reader.GetOrdinal("userID")),
-                            Name = reader.GetString(reader.GetOrdinal("name")),
-                            Address = reader.GetString(reader.GetOrdinal("address")),
-                            PhoneNumber = reader.GetString(reader.GetOrdinal("phone_number")),
-                            Email = reader.GetString(reader.GetOrdinal("email")),
-                            Type = reader.GetString(reader.GetOrdinal("type"))
-                        };
+                        cmd.Parameters.AddWithValue("@name", user.Name);
+                        cmd.Parameters.AddWithValue("@address", user.Address);
+                        cmd.Parameters.AddWithValue("@phone_number", user.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@email", user.Email);
+                        cmd.Parameters.AddWithValue("@password", user.Password);
+                        cmd.Parameters.AddWithValue("@type", user.Type);
+                        await cmd.ExecuteNonQueryAsync();
                     }
                 }
             }
-        }
-        return user;
-    }
-
-    public async Task<List<User>> GetUsersAsync()
-    {
-        var users = new List<User>();
-        using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
-        {
-            await conn.OpenAsync();
-            string query = "SELECT * FROM user_account";
-            using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+            catch (Exception ex)
             {
-                using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
+                throw new Exception("Error during registration: " + ex.Message, ex);
+            }
+        }
+
+        public async Task<User> LoginAsync(string email, string password)
+        {
+            User user = null;
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+                string query = "SELECT * FROM LoginUser(@Email, @Password)";
+                using (var cmd = new NpgsqlCommand(query, conn))
                 {
-                    while (await reader.ReadAsync())
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    using (var reader = await cmd.ExecuteReaderAsync())
                     {
-                        users.Add(new User
+                        if (await reader.ReadAsync())
                         {
-                            UserID = reader.GetInt32(0),
-                            Name = reader.GetString(1),
-                            Address = reader.GetString(2),
-                            PhoneNumber = reader.GetString(3),
-                            Email = reader.GetString(4)
-                        });
+                            user = new User
+                            {
+                                UserID = reader.GetInt32(reader.GetOrdinal("userID")),
+                                Name = reader.GetString(reader.GetOrdinal("name")),
+                                Address = reader.GetString(reader.GetOrdinal("address")),
+                                PhoneNumber = reader.GetString(reader.GetOrdinal("phone_number")),
+                                Email = reader.GetString(reader.GetOrdinal("email")),
+                                Type = reader.GetString(reader.GetOrdinal("type"))
+                            };
+                        }
                     }
                 }
             }
+            return user;
         }
-        return users;
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var users = new List<User>();
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+                string query = "SELECT * FROM user_account";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            users.Add(new User
+                            {
+                                UserID = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                Address = reader.GetString(2),
+                                PhoneNumber = reader.GetString(3),
+                                Email = reader.GetString(4)
+                            });
+                        }
+                    }
+                }
+            }
+            return users;
+        }
     }
 }
