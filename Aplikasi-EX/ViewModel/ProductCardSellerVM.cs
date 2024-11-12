df06a46b4772e51306c2d9de773d3d756376537c
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-11-13 00:16:07 +0700

    feat

---
+++ Aplikasi-EX/ViewModel/ProductCardSellerVM.cs
@@ -0,0 +1,19 @@
+﻿using Aplikasi_EX.Model;
+using Aplikasi_EX.Utilities;
+using Aplikasi_EX.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Aplikasi_EX.ViewModel
+{
+    public class ProductCardSellerVM 
+    {
+
+
+    }
+}
