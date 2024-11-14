877de5b2294d5ce94f2f14ffbf15da22ac41a542
rhnathar <zildan.athar@gmail.com>
2024-11-14 07:37:33 +0700

    fix: splitting buyer and seller account vm

--- Aplikasi-EX/ViewModel/SellerAccountVM.cs
+++ Aplikasi-EX/ViewModel/SellerAccountVM.cs
@@ -1,65 +1,118 @@
 ﻿using System;
+using Aplikasi_EX.DataAccess;
+using System.Collections.ObjectModel;
 using Aplikasi_EX.Model;
 using Aplikasi_EX.Utilities;
+using System.Threading.Tasks;
 
 namespace Aplikasi_EX.ViewModel
 {
     class SellerAccountVM : Utilities.ViewModelBase
     {
-        private string _greeting;
-        private string _fullName;
-        private string _address;
-        private string _email;
-        private string _username;
-        private string _phoneNumber;
+        private readonly OrderRepository _orderRepository;
 
-        // Properties for seller account details
-        public string Greeting
+        private User _currentUser;
+
+        public User CurrentUser
         {
-            get => _greeting;
-            set { _greeting = value; OnPropertyChanged(nameof(Greeting)); }
+            get => _currentUser;
+            set { _currentUser = value; OnPropertyChanged(nameof(CurrentUser)); }
         }
+
+        // Orders collection
+        private ObservableCollection<Order> _orders;
+        public ObservableCollection<Order> Orders
+        {
+            get => _orders;
+            set { _orders = value; OnPropertyChanged(nameof(Orders)); }
+        }
+
+
+        // Properties for user details
+        public string Greeting => $"Hello, {CurrentUser?.Name}";
 
         public string FullName
         {
-            get => _fullName;
-            set { _fullName = value; OnPropertyChanged(nameof(FullName)); }
+            get => CurrentUser?.Name;
+            set
+            {
+                if (CurrentUser != null)
+                {
+                    CurrentUser.Name = value;
+                    OnPropertyChanged(nameof(FullName));
+                }
+            }
         }
 
         public string Address
         {
-            get => _address;
-            set { _address = value; OnPropertyChanged(nameof(Address)); }
+            get => CurrentUser?.Address;
+            set
+            {
+                if (CurrentUser != null)
+                {
+                    CurrentUser.Address = value;
+                    OnPropertyChanged(nameof(Address));
+                }
+            }
         }
 
         public string Email
         {
-            get => _email;
-            set { _email = value; OnPropertyChanged(nameof(Email)); }
+            get => CurrentUser?.Email;
+            set
+            {
+                if (CurrentUser != null)
+                {
+                    CurrentUser.Email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
         }
 
         public string Username
         {
-            get => _username;
-            set { _username = value; OnPropertyChanged(nameof(Username)); }
+            get => CurrentUser?.Name; // Assuming Username is the same as Name
+            set
+            {
+                if (CurrentUser != null)
+                {
+                    CurrentUser.Name = value;
+                    OnPropertyChanged(nameof(Username));
+                }
+            }
         }
 
         public string PhoneNumber
         {
-            get => _phoneNumber;
-            set { _phoneNumber = value; OnPropertyChanged(nameof(PhoneNumber)); }
+            get => CurrentUser?.PhoneNumber;
+            set
+            {
+                if (CurrentUser != null)
+                {
+                    CurrentUser.PhoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
         }
 
         // Constructor to initialize dummy data
         public SellerAccountVM()
         {
-            // Dummy data for testing
-            Greeting = "Halo, Seller Kevin";
-            FullName = "Kevin Gilbert";
-            Address = "East Tejturi Bazar, Word No. 04, Road No. 13/x, House no. 1320/C, Flat No. 5D, Dhaka - 1200, Bangladesh";
-            Email = "seller.kevin@gmail.com";
-            Username = "seller_kevin";
-            PhoneNumber = "+1-202-555-0123";
+            _orderRepository = new OrderRepository();
+            if (UserSession.IsUserLoggedIn)
+            {
+                CurrentUser = UserSession.CurrentUser;
+                Task.Run(async () => await LoadOrders());
+            }
+        }
+        private async Task LoadOrders()
+        {
+            if (CurrentUser != null)
+            {
+                var ordersFromDb = await _orderRepository.getSellerOrderAsync(CurrentUser.UserID);
+                Orders = new ObservableCollection<Order>(ordersFromDb);
+            }
         }
     }
 }
