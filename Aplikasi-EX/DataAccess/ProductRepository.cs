2010a25943d13659f576c05777fe9c843564b00d
rhnathar <zildan.athar@gmail.com>
2024-11-17 11:30:53 +0700

    Revert "feat: add deleteproduct, updateproduct access"
    This reverts commit ccc2e33e22e8f52d2b5db603488a385a8b1f148b.

--- Aplikasi-EX/DataAccess/ProductRepository.cs
+++ Aplikasi-EX/DataAccess/ProductRepository.cs
@@ -125,35 +125,14 @@ namespace Aplikasi_EX.DataAccess
             }
             return products;
         }
-<<<<<<< HEAD
-
-        public async Task UpdateProductAsync(Product product)
-        {
-=======
         public async Task<List<Product>> GetProductsBySearchAsync(string search)
         {
             var products = new List<Product>();
->>>>>>> 3bafb097e69546b5186dcf437f117526933e3530
             try
             {
                 using (var conn = new NpgsqlConnection(_connectionString))
                 {
                     await conn.OpenAsync();
-<<<<<<< HEAD
-                    string query = "SELECT updateproduct(@productID, @product_name, @price, @date_added, @description, @quantity, @condition, @category, @product_image)";
-                    using (var cmd = new NpgsqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@productID", product.ProductID);
-                        cmd.Parameters.AddWithValue("@product_name", product.ProductName);
-                        cmd.Parameters.AddWithValue("@price", product.Price);
-                        cmd.Parameters.AddWithValue("@date_added", product.DateAdded);
-                        cmd.Parameters.AddWithValue("@description", product.Description);
-                        cmd.Parameters.AddWithValue("@quantity", product.Quantity);
-                        cmd.Parameters.AddWithValue("@condition", product.Condition);
-                        cmd.Parameters.AddWithValue("@category", product.Category);
-                        cmd.Parameters.AddWithValue("@product_image", product.ImagePath);
-                        await cmd.ExecuteNonQueryAsync();
-=======
                     string query = "SELECT * FROM getproductbysearch(@search)";
                     using (var cmd = new NpgsqlCommand(query, conn))
                     {
@@ -177,66 +156,14 @@ namespace Aplikasi_EX.DataAccess
                                 });
                             }
                         }
->>>>>>> 3bafb097e69546b5186dcf437f117526933e3530
                     }
                 }
             }
             catch (Exception ex)
             {
-<<<<<<< HEAD
-                throw new Exception("Error during update product: " + ex.Message, ex);
-            }
-        }
-        public async Task DeleteProductAsync(Product product)
-        {
-            try
-            {
-                using (var conn = new NpgsqlConnection(_connectionString))
-                {
-                    await conn.OpenAsync();
-                    string query = "SELECT deleteproduct(@productID)";
-                    using (var cmd = new NpgsqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@productID", product.ProductID);
-                        await cmd.ExecuteNonQueryAsync();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error during product deletion: " + ex.Message, ex);
-            }
-        }
-
-        public async Task GetProductByIdAsync(Product product)
-        {
-            try
-            {
-                using (var conn =new NpgsqlConnection(_connectionString))
-                {
-                    await conn.OpenAsync();
-                    string query = "SELECT getproductbyid(@productID)";
-                    using (var cmd = new NpgsqlCommand(query,conn))
-                    {
-                        cmd.Parameters.AddWithValue("@productID", product.ProductID);
-                        await cmd.ExecuteNonQueryAsync();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error during product getting: " + ex.Message, ex);
-            }
-        }
-
-        internal async Task GetProductByIdAsync(int productID, int productID1)
-        {
-            throw new NotImplementedException();
-=======
                 throw new Exception("Error retrieving seller products: " + ex.Message, ex);
             }
             return products;
->>>>>>> 3bafb097e69546b5186dcf437f117526933e3530
         }
     }
 }
